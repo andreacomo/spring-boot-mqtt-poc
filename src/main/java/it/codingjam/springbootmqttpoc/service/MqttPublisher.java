f38498d89e428c446237df8c732f7b210480434e
Andrea Como <andrea.como@gmail.com>
2026-04-19 18:01:11 +0200

    service: add mqtt publisher service

---
+++ src/main/java/it/codingjam/springbootmqttpoc/service/MqttPublisher.java
@@ -0,0 +1,39 @@
+package it.codingjam.springbootmqttpoc.service;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Service
+public class MqttPublisher {
+    private static final Logger logger = LoggerFactory.getLogger(MqttPublisher.class);
+    private final MqttClient mqttClient;
+
+    public MqttPublisher(MqttClient mqttClient) {
+        this.mqttClient = mqttClient;
+    }
+
+    public void publish(String topic, String message, int qos) throws MqttException {
+        try {
+            if (!mqttClient.isConnected()) {
+                throw new MqttException(new Exception("Broker not connected"));
+            }
+
+            MqttMessage mqttMessage = new MqttMessage(message.getBytes());
+            mqttMessage.setQos(qos);
+            mqttClient.publish(topic, mqttMessage);
+
+            logger.info("Published message to topic {}: {}", topic, message);
+        } catch (MqttException e) {
+            logger.error("Failed to publish message to {}: {}", topic, e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    public boolean isConnected() {
+        return mqttClient.isConnected();
+    }
+}
