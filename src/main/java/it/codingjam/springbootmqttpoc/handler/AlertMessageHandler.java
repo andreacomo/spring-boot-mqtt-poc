e430c07dcdec7aa81faed1f99cca40183d4e4f22
Andrea Como <andrea.como@gmail.com>
2026-04-19 17:58:30 +0200

    handler: add alert message handler

---
+++ src/main/java/it/codingjam/springbootmqttpoc/handler/AlertMessageHandler.java
@@ -0,0 +1,28 @@
+package it.codingjam.springbootmqttpoc.handler;
+
+import it.codingjam.springbootmqttpoc.config.MqttProperties;
+import it.codingjam.springbootmqttpoc.service.MessageStorage;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+public class AlertMessageHandler extends MqttMessageHandlerBase {
+    private static final Logger logger = LoggerFactory.getLogger(AlertMessageHandler.class);
+
+    public AlertMessageHandler(MessageStorage messageStorage, MqttClient mqttClient, MqttProperties mqttProperties) {
+        super(messageStorage, mqttClient, mqttProperties);
+    }
+
+    @Override
+    protected String getConfigKey() {
+        return "alerts";
+    }
+
+    @Override
+    protected void handleMessage(String payload) throws Exception {
+        logger.warn("Handling alert: {}", payload);
+        // Add business logic here
+    }
+}
