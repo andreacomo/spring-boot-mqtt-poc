3c23de467c983dacbfd98b4ab98646b47876c6c5
Andrea Como <andrea.como@gmail.com>
2026-04-19 17:57:51 +0200

    handler: add command message handler

---
+++ src/main/java/it/codingjam/springbootmqttpoc/handler/CommandMessageHandler.java
@@ -0,0 +1,28 @@
+package it.codingjam.springbootmqttpoc.handler;
+
+import it.codingjam.springbootmqttpoc.config.MqttProperties;
+import it.codingjam.springbootmqttpoc.service.MessageStorage;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+public class CommandMessageHandler extends MqttMessageHandlerBase {
+    private static final Logger logger = LoggerFactory.getLogger(CommandMessageHandler.class);
+
+    public CommandMessageHandler(MessageStorage messageStorage, MqttClient mqttClient, MqttProperties mqttProperties) {
+        super(messageStorage, mqttClient, mqttProperties);
+    }
+
+    @Override
+    protected String getConfigKey() {
+        return "commands";
+    }
+
+    @Override
+    protected void handleMessage(String payload) throws Exception {
+        logger.info("Executing command: {}", payload);
+        // Add business logic here
+    }
+}
